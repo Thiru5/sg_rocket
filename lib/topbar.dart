d0e092b7e90df4ae2490b3bd7ddae294cc99d24e
Exi0n97 <48261015+Exi0n97@users.noreply.github.com>
2020-04-01 17:02:33 +0800

    title of branch

--- lib/topbar.dart
+++ lib/topbar.dart
@@ -2,26 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:sg_rocket/main.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 class TopBar extends StatelessWidget {
-
   LatLng startLocation;
   LatLng destination;
   String startLocationName;
   String destinationName;
-  TopBar({Key key, @required this.destination,this.startLocation,this.startLocationName,this.destinationName}) : super(key: key);
-
+  TopBar(
+      {Key key,
+      @required this.destination,
+      this.startLocation,
+      this.startLocationName,
+      this.destinationName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    if(startLocationName == null)
-    {
+    if (startLocationName == null) {
       startLocationName = 'Proceed Back to Select';
     }
 
-    if(destinationName == null)
-    {
+    if (destinationName == null) {
       destinationName = 'Proceed Back to Select';
     }
 
@@ -32,60 +32,73 @@ class TopBar extends StatelessWidget {
           MaterialPageRoute(builder: (context) => HomePage()),
         );
       },
-      child: Container(
-        alignment: Alignment.topCenter,
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 25),
-            Row(
-              children: <Widget>[
-                Text(
-                  '  From',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.0,
+      child: Scaffold(
+              body: Container(
+          alignment: Alignment.topCenter,
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 25),
+              Row(
+                children: <Widget>[
+                  Text(
+                    '  From',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                    ),
                   ),
-                ),
-                SizedBox(width: 20),
-                Text(
-                  startLocationName,
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.0,
+                  SizedBox(width: 20),
+                  Flexible(
+                    child: Container(
+                      child: Text(
+                        startLocationName,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.0,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 5),
-            Row(
-              children: <Widget>[
-                Text(
-                  '  To  ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.0,
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: <Widget>[
+                  Text(
+                    '  To  ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                    ),
                   ),
-                ),
-                SizedBox(width: 20),
-                Text(
-                  destinationName,
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.0,
+                  SizedBox(width: 20),
+                  Flexible(
+                    child: Container(
+                      child: Text(
+                        destinationName,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.0,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
