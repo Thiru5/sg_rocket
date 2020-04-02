44bf27f9cc944f605d7f560c03fb1c4910e457a8
B THIRUKARTHIK <thirukarthik95@gmail.com>
2020-04-02 16:28:53 +0800

    Working database

--- lib/topbar.dart
+++ lib/topbar.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:sg_rocket/models/database.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:sg_rocket/data_list.dart';
 import 'package:sg_rocket/models/routeQuery.dart';
 
+import 'main.dart';
 
 class TopBar extends StatelessWidget {
 //  LatLng startLocation;
@@ -15,6 +15,16 @@ class TopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String startAdd;
+    String endAdd;
+
+    final locationQuery = Provider.of<List<RouteQuery>>(context);
+    locationQuery.forEach((locationQuery) {
+      endAdd = locationQuery.endAddress;
+      print(locationQuery.endPoint);
+      startAdd = locationQuery.startAddress;
+      print(locationQuery.startPoint);
+    });
 //    if(startLocationName == null)
 //    {
 //      startLocationName = 'Proceed Back to Select';
@@ -26,102 +36,71 @@ class TopBar extends StatelessWidget {
 //    }
 
     return StreamProvider<List<RouteQuery>>.value(
-        value: LocationQuery().routeQuery,
-        child: Scaffold(
-          body: DataList(),
-        )
-//            child: GestureDetector(
-//              onTap: () {
-//                Navigator.push(
-//                  context,
-//                  MaterialPageRoute(builder: (context) => HomePage()),
-//                );
-//              },
-//
-//              child: Container(
-//                alignment: Alignment.topCenter,
-//                color: Colors.white,
-//                child: Column(
-//                  children: <Widget>[
-//                    FlatButton(color: Colors.blue,
-//                        onPressed: (){
-//                      print('pressed');
-//
-//                        }
-//                    ),
-//                    SizedBox(height: 25),
-//                    Row(
-//                      children: <Widget>[
-//                        Text(
-//                          '  From',
-//                          style: TextStyle(
-//                            color: Colors.black,
-//                            fontSize: 16.0,
-//                            fontWeight: FontWeight.w600,
-//                            letterSpacing: 1.0,
-//                          ),
-//                        ),
-//                        SizedBox(width:20),
-//
-//                        //                Text(
-//
-//                        //                  startLocationName,
-//
-//                        //                  style: TextStyle(
-//
-//                        //                    color: Colors.green,
-//
-//                        //                    fontSize: 16.0,
-//
-//                        //                    fontWeight: FontWeight.w600,
-//
-//                        //                    letterSpacing: 1.0,
-//
-//                        //                  ),
-//
-//                        //                ),
-//                      ],
-//                    ),
-//                    SizedBox(height: 5),
-//                    Row(
-//                      children: <Widget>[
-//                        Text(
-//                          '  To  ',
-//                          style: TextStyle(
-//                            color: Colors.black,
-//                            fontSize: 16.0,
-//                            fontWeight: FontWeight.w600,
-//                            letterSpacing: 1.0,
-//                          ),
-//                        ),
-//
-//                        SizedBox(width: 20),
-//
-//                        //                Text(
-//
-//                        //                  destinationName,
-//
-//                        //                  style: TextStyle(
-//
-//                        //                    color: Colors.red,
-//
-//                        //                    fontSize: 16.0,
-//
-//                        //                    fontWeight: FontWeight.w600,
-//
-//                        //                    letterSpacing: 1.0,
-//
-//                        //                  ),
-//
-//                        //                ),
-//                      ],
-//                    ),
-//
-//                  ],
-//
-//                ),
-//              ),
-//            ),
+      value: LocationQuery().routeQuery,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        },
+        child: Container(
+          alignment: Alignment.topCenter,
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 25),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'From',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    startAdd,
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: <Widget>[
+                  Text(
+                    '  To  ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    endAdd,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
