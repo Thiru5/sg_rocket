da62e6b7d3ca0fa74677430f7de50f745dbb7d6d
B THIRUKARTHIK <thirukarthik95@gmail.com>
2020-04-03 06:36:30 +0800

    Remove

--- lib/option_menu.dart
+++ lib/option_menu.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sg_rocket/display_route_ui.dart';
 
 import 'package:sg_rocket/fastest_route.dart';
 import 'package:sg_rocket/lonely_route.dart';
